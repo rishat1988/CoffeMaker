f2a6d3086883b5cdc444daa3c294cf15edcc416c
Rishat <r_man1@mail.ru>
2021-01-16 10:38:59 +0300

    Task centralServis

--- centralservers/server/src/main/java/com/example/server/ServerApplication.java
+++ centralservers/server/src/main/java/com/example/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.centralservers;
+package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class CentralserversApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CentralserversApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
