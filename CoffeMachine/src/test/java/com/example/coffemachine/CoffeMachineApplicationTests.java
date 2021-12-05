763fcf3796bbe247b0988c41f533e93122492ae6
Rishat <r_man1@mail.ru>
2021-12-05 17:06:43 +0300

    create structure of programm

---
+++ CoffeMachine/src/test/java/com/example/coffemachine/CoffeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
